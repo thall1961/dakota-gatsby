81ec59d288c1a71580ab11f7838e3bc04da09be9
thall1961 <thall1961@gmail.com>
2020-10-05 13:32:04 -0600

    menu close after select

--- src/pages/index.js
+++ src/pages/index.js
@@ -78,91 +78,8 @@ if (typeof window !== 'undefined' && typeof document !== 'undefined') {
             $('.nav-menu').slideToggle();
         });
 
-        // ========================================================================= //
-        //  Typed Js
-        // ========================================================================= //
-
-        // let typed = $(".typed");
-        //
-        // $(function() {
-        //   typed.typed({
-        //     strings: ["Alex Smith.", "Designer.", "Developer.", "Freelancer.", "Photographer"],
-        //     typeSpeed: 100,
-        //     loop: true,
-        //   });
-        // });
-
-
-        // ========================================================================= //
-        //  Owl Carousel Services
-        // ========================================================================= //
-
-
-        // $('.services-carousel').owlCarousel({
-        //   autoplay: true,
-        //   loop: true,
-        //   margin: 20,
-        //   dots: true,
-        //   nav: false,
-        //   responsiveClass: true,
-        //   responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
-        // });
-
-        // ========================================================================= //
-        //  magnificPopup
-        // ========================================================================= //
-
-        // let magnifPopup = function() {
-        //   $('.popup-img').magnificPopup({
-        //     type: 'image',
-        //     removalDelay: 300,
-        //     mainClass: 'mfp-with-zoom',
-        //     gallery: {
-        //       enabled: true
-        //     },
-        //     zoom: {
-        //       enabled: true, // By default it's false, so don't forget to enable it
-        //
-        //       duration: 300, // duration of the effect, in milliseconds
-        //       easing: 'ease-in-out', // CSS transition easing function
-        //
-        //       // The "opener" function should return the element from which popup will be zoomed in
-        //       // and to which popup will be scaled down
-        //       // By defailt it looks for an image tag:
-        //       opener: function(openerElement) {
-        //         // openerElement is the element on which popup was initialized, in this case its <a> tag
-        //         // you don't need to add "opener" option if this code matches your needs, it's defailt one.
-        //         return openerElement.is('img') ? openerElement : openerElement.find('img');
-        //       }
-        //     }
-        //   });
-        // };
-
-
-        // Call the functions
-        // magnifPopup();
-
     });
 }
-// ========================================================================= //
-//  Porfolio isotope and filter
-// ========================================================================= //
-// $(window).load(function(){
-//
-//   let portfolioIsotope = $('.portfolio-container').isotope({
-//     itemSelector: '.portfolio-thumbnail',
-//     layoutMode: 'fitRows'
-//   });
-//
-//   $('#portfolio-flters li').on( 'click', function() {
-//     $("#portfolio-flters li").removeClass('filter-active');
-//     $(this).addClass('filter-active');
-//
-//     portfolioIsotope.isotope({ filter: $(this).data('filter') });
-//   });
-//
-// })
-
 
 export default function Home() {
     const data = useStaticQuery(graphql`
@@ -212,9 +129,9 @@ export default function Home() {
         }
     `)
 
-    const handleClick = (link) => {
-        console.log('link', link);
-        navigate(link);
+    const handleClick = async (link) => {
+        await navigate(link);
+        $('.nav-menu').slideToggle();
     }
 
     return (
