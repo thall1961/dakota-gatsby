d0f98486b0b65c86c32990d540d70c63ae5fc860
thall1961 <thall1961@gmail.com>
2021-08-30 13:17:37 -0600

    taking out what i dont need

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,6 @@ module.exports = {
         },
         `gatsby-plugin-sharp`,
         `gatsby-transformer-sharp`,
-        `gatsby-plugin-styled-components`,
         {
             resolve: `gatsby-plugin-google-gtag`,
             options: {
